d804967003b07b43a37334539a9514d1dbe12cdb
Andrianarisoa Daniel <radandevist@gmail.com>
2022-02-04 10:40:24 +0300

    🚧 work in progress: drawer base activity

---
+++ app/src/main/java/com/mem/mqos/DrawerBaseActivity.kt
@@ -0,0 +1,11 @@
+package com.mem.mqos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrawerBaseActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_drawer_base)
+  }
+}
