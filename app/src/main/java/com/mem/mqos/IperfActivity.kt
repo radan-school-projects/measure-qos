203ff980ba3215880f8180013868212de53bc828
Andrianarisoa Daniel <radandevist@gmail.com>
2022-02-04 09:16:03 +0300

    ✨ initial commit: Iperf Activity on launch

---
+++ app/src/main/java/com/mem/mqos/IperfActivity.kt
@@ -0,0 +1,11 @@
+package com.mem.mqos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IperfActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_iperf)
+  }
+}
